64b1e5e4bfef850f20db8b59172ef78e931cf5d4
Jolazzz <mega12c88@gmail.com>
2025-06-03 14:20:38 +0800

    Add files via upload

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,6 +9,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/twrp_larry.mk
 
 COMMON_LUNCH_CHOICES := \
-   twrp_larry-user \
-   twrp_larry-userdebug \
+    twrp_larry-user \
+    twrp_larry-userdebug \
     twrp_larry-eng
